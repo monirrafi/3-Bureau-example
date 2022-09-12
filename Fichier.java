6e3e60ee64aa879e2e94cbc8559f708b926d1855
monirrafi <100225631+monirrafi@users.noreply.github.com>
2022-09-12 13:48:43 -0400

    save

---
+++ Fichier.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Fichier
+{
+public static void main (String [] argv) throws IOException
+{
+RandomAccessFile f = new RandomAccessFile ("fic.dat", "rw");
+f.writeInt (34);
+f.writeDouble (3.14159);
+f.writeBytes ("Bonjour\n");
+f.writeChars ("ce serait un bon squelette");
+f.seek (0); //f.length ()
+int i = f.readInt ();
+double g = f.readDouble ();
+String str = f.readLine ();
+System.out.println (i + "");
+System.out.println (g);
+System.out.println (str);
+f.close ();
+} // main ()
+} //Fichier
